14232f9d91711f2b21442e3531622210e9c9c21e
Venkatasubramanian S <60696284+chandragupta1888@users.noreply.github.com>
2022-09-20 17:58:13 +0530

    Add files via upload

---
+++ PC-BOSS.bat
@@ -0,0 +1,80 @@
+@echo off
+title BOSS-PC
+color a
+                                                
+                                                                                                    
+echo                                              EEED  DE:                                             
+echo                                             EEEEEEEEEE                                             
+echo                                            EEEEEEEEEEED                                            
+echo                                           EEEEEEEEEEEEE                                            
+echo                                          .EEEEEEEEEEEEEE                                           
+echo                                          EEEEEEEEEEEEEEE                                           
+echo                                         LEEEEEEEEEEEEEEEE                                          
+echo                                         EEEEEEEEEEEEEEEEE                                          
+echo                                        EEEEEEEEEEEEEEEEEEE                                         
+echo                                        EEEEEEEEEEEEEEEEEEE                                         
+echo                                       EEEEEEEEEEEEEEEEEEEEE                                        
+echo                                       EEEEEEEEEEEEEEEEEEEEE                                        
+echo                                .LEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEL:                                 
+echo                             EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                              
+echo                              .EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                                
+echo                                  DEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                                   
+echo                                     GGEEEEEEEEEEEEEEEEEEEEEED                                      
+echo                                         fEEEEEEEEEEEEEEEL                                          
+echo                                             tEEEEEEEt                                              
+echo                                          EEEED      DEEEE                                          
+echo                                         EEEEEEE.  :EEEEEEE  E                                      
+echo                                        EEEEEEEEE  EEEEEEEEE EE                                     
+echo                                    EED EEEEEEEEEDDEEEEEEEEE EEE                                    
+echo                                   EEEE EEEEEEEE:  :EEEEEEEE EEEE.                                  
+echo                                  EEEEE EEEEEEEE    EEEEEEEE EEEEEj                                 
+echo                                ,EEEEEE DEEEEEE      EEEEEEG EEEEEEE                                
+echo                               ;EEEEEEEf DEEEE        EEEED ,EEEEEEEE                               
+echo                               EEEEEEEEE                    EEEEEEEEE.                              
+echo                                .EEEEEEE                    EEEEEEEL                                
+echo                                  .EEEEE;                   EEEEEL                                  
+echo                                    LEEEE                  EEEEE                                    
+echo                                      DEEi                 EEE                                      
+echo                                    DEEEEE                EEEEEE,                                   
+echo                                 EEEEEEEEEE              DEEEEEEEEE                                 
+echo                                EEEEEEEEEEED            fEEEEEEEEEEEt                               
+echo                              :EEEEEEEEEEEEEE          EEEEEEEEEEEEEED                              
+echo                              EEEEEEEEEEEEEEEEEj    ;EEEEEEEEEEEEEEEEE                              
+echo                             ,EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                             
+echo                             EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                             
+echo                             EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                             
+echo                            EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE                            
+echo                            EEEE ...................................DEEE                            
+echo                           .EEEE.EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE.                           
+echo                           EEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEDEEEEE                           
+echo                           EEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEfEEEEE                           
+echo                          :EEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE.EEEEE:                          
+echo                          EEEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EEEEEE                          
+echo                          EEEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EEEEEE                          
+echo                         fEEEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EEEEEEL                         
+echo                         EEEEEEE EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EEEEEEE                         
+echo                         EEEEEEE.EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EEEEEEE                         
+echo                         EEEEEEEjEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EEEEEEE                         
+echo                         EEEEEEEGEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE EEEEEEE                         
+echo                         EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE.EEEEEEE                         
+echo                          EEEEEEEDEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEiEEEEEE                          
+echo                           EEEEEEjEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEGEEEEE                           
+echo                             DEEE,EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED                             
+echo                                E EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEDE                                
+echo                                                                    ,
+echo                                                                    ,                                                                         
+echo                              =======================================
+echo                                  Written by Venkatasubramanian S
+echo                              mail : venkat2002subramanian@gmail.com
+echo                              =======================================
+                                                                                         
+                                                  
+pause
+Rem explorer.exe Shell:::{2559a1f3-21d7-11d4-bdaf-00c04f60b9f0}
+Rem Set WshShell = WScript.CreateObject("WScript.Shell")
+Rem WshShell.Sendkeys "{exit}"
+Rem {^}+A {DEL} 
+ipconfig/displaydns
+echo By pressing any key will flush the DNS records
+pause
+ipconfig/flushdns
